fa13c94a037ae2877a4222840e84c8b4e99a7fe2
caioconti <caioconti1@gmail.com>
2019-08-10 14:46:58 -0300

    09/08 - Funcionalidades avançadas

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -16,7 +16,16 @@ module.exports = {
     //Verifica se o loggedDev(Dev dando like) está na lista de likes do targetDev(Dev recebendo like),
     // pra ver se da MATCH
     if (targetDev.likes.includes(loggedDev._id)) {
-      console.log('DEU MATCH');
+      const loggedSocket = req.connectedUsers[user];
+      const targetSocket = req.connectedUsers[devId];
+
+      if (loggedSocket) {
+        req.io.to(loggedSocket).emit('match', targetDev);
+      }
+
+      if (targetSocket) {
+        req.io.to(targetSocket).emit('match', loggedDev);
+      }
     }
 
     //Pegando o id targetDev(usuário que está recebendo o like) e add na lista de likes do Dev logado
